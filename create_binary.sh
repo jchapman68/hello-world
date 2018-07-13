4b7f186f65412c1d776a46f6b374a89f5c0309db
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-13 11:33:28 +0100

    add set -x to output commands to log

--- create_binary.sh
+++ create_binary.sh
@@ -3,6 +3,9 @@
 # set command to fail if any subsequent command fails, saves individual error trapping for each command
 set -e
 
+# echo commands to show in log
+set -x
+
 cp hello-world-git/Procfile artifact/Procfile
 cd hello-world-git
 go build -o ../artifact/helloworld
